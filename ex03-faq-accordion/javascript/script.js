942fed296653d1d39b32810d0d609b7a8502951e
Tamires Sena <101232103+tamiressena@users.noreply.github.com>
2024-09-10 16:06:52 -0300

    remover função não utilizada

--- ex03-faq-accordion/javascript/script.js
+++ ex03-faq-accordion/javascript/script.js
@@ -4,11 +4,5 @@ accordions.forEach((accordion) => {
   accordion.addEventListener("click", () => {
     const body = accordion.querySelector(".accordion-body");
     body.classList.toggle("active");
-
-    if (document.getElementById("click").src == "https://flic.kr/p/2qfyozU") {
-      document.getElementById("click").src = "https://flic.kr/p/2qfszyJ";
-    } else {
-      document.getElementById("click").src = "https://flic.kr/p/2qfyozU";
-    }
   });
 });
